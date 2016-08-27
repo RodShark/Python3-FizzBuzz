7b84ad6818f0b7ab4097490b84be0ef80db7be26
Roderick Sang <rodericksang@gmail.com>
2016-08-27 16:57:39 +0000

    A fizzbuzz project, not yet complete because of an error I'm having. First project I ever uploaded using Git and not just GitHub.

---
+++ fizzbuzzproject.py
@@ -0,0 +1,39 @@
+import sys
+
+def main():
+    print("Welcome to the FizzBuzz game!")
+    print("In this particular game, you set the limit for how high we'll go!")
+    print("According to what you typed, we'll be going only up to {}.".format(limit))
+    
+    for number in range(1, int(limit)+1):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz!")
+        elif number % 3 == 0:
+            print("Fizz!")
+        elif number % 5 == 0:
+            print("Buzz!")
+        else:
+            print(number)
+
+if len(sys.argv) <= 1:
+    limit = input("Give me a number to set as the highest value we'll be going to. ")
+
+ # This try statement doesn't work at all. I couldn't figure it out...   
+    try:
+        int(limit)
+    except ValueError:
+        print("Please give me a numeric number, not a word. :)")
+        sys.exit()
+        
+    main()
+    
+else:
+    limit = sys.argv[1]
+    
+    try:
+        int(limit)
+    except ValueError:
+        print("Please give me a numeric number, not a word. :)")
+        sys.exit()
+        
+    main()
